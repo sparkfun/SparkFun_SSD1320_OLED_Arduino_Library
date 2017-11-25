1a78ab04691fd90218c20d8d21fd7f0024a41c71
Nathan Seidle <nathan@sparkfun.com>
2017-11-25 10:29:33 -0700

    Adding pointers to python script that generated the progmem array

--- examples/Example2_Graphics/Example2_Graphics.ino
+++ examples/Example2_Graphics/Example2_Graphics.ino
@@ -5,6 +5,14 @@
   Date: November 21st, 2017
   License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
 
+  The flexible OLED is grayscale meaning we can display some neat looking images. To do this
+  we need grayscale information. This example takes the raw bytes from a header file and sends
+  them to the display. Uncomment either macaque.h or che.h and upload the program to see
+  the image on the OLED.
+
+  The header files were generated using a python script. See for details:
+  https://github.com/sparkfun/BMPtoArray
+
   To connect the display to an Arduino:
   (Arduino pin) = (Display pin)
   Pin 13 = SCLK on display carrier
@@ -19,7 +27,8 @@
 */
 
 #include <SSD1320_OLED.h>
-#include "image.h" //Raw bytes of the Macaque monkey selfie
+#include "macaque.h" //Raw bytes of the Macaque monkey selfie
+//#include "che.h" //Raw bytes of the comic che
 
 //Initialize the display with the follow pin connections
 SSD1320 flexibleOLED(10, 9); //10 = CS, 9 = RES
@@ -35,12 +44,10 @@ void setup()
   flexibleOLED.setRowAddress(0);
   flexibleOLED.setColumnAddress(0);
 
-  //We have 160 pixels. Each pixel is 4-bit so we have 80 bytes wide.
-  //Display is 32 rows tall so 32 * 80 = 2,560 bytes to write.
-  for (int i = 0 ; i < 2560 ; i++)
+  //Send the bytes from program memory to OLED display
+  for (int i = 0 ; i < sizeof(myGraphic) ; i++)
   {
-    byte theByte = pgm_read_byte(LCD_graphic + 2559 - i);
-    theByte ^= 0xFF; //We must invert the byte because the software converted it backwards
+    byte theByte = pgm_read_byte(myGraphic + i);
     flexibleOLED.data(theByte); //Write byte directly to display
   }
 }
